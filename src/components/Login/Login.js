424c0b66c975e791757943595e4f05baafbbbc3e
johirjohnny <johirjohnny96@gmail.com>
2021-03-19 01:36:55 +0600

    final commit

---
+++ src/components/Login/Login.js
@@ -0,0 +1,54 @@
+import React, { useContext } from 'react';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+import { userContext } from '../../App';
+import { useHistory, useLocation } from 'react-router';
+
+
+const Login = () => {
+    const [loggedInUser, setLoggedInUser] = useContext(userContext);
+    const history = useHistory();
+    const location = useLocation();
+    const { from } = location.state || { from: { pathname: "/" } };
+    if (firebase.app.length === 0) {
+        firebase.initializeApp(firebaseConfig);
+    }
+
+
+    const handleGoogleSignIn = () => {
+        var provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth()
+            .signInWithPopup(provider)
+            .then((result) => {
+                /** @type {firebase.auth.OAuthCredential} */
+                var credential = result.credential;
+
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                var token = credential.accessToken;
+                // The signed-in user info.
+                const { displayName, email } = result.user;
+                const signedInUser = { name: displayName, email };
+                setLoggedInUser(signedInUser);
+                history.replace(from);
+                // ...
+            }).catch((error) => {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                // The email of the user's account used.
+                var email = error.email;
+                // The firebase.auth.AuthCredential type that was used.
+                var credential = error.credential;
+                // ...
+            });
+    }
+    return (
+        <div>
+            <h1>This is Login</h1>
+            <button onClick={handleGoogleSignIn}>Google Sign in</button>
+        </div>
+    );
+};
+
+export default Login;
---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDgBEeRvEZTbrwvG7DfkJMl-sFC44dUtPQ",
+    authDomain: "burj-al-arab-79014.firebaseapp.com",
+    projectId: "burj-al-arab-79014",
+    storageBucket: "burj-al-arab-79014.appspot.com",
+    messagingSenderId: "415672893338",
+    appId: "1:415672893338:web:f4dce286c3ed43d2abc423"
+  };
+  export default firebaseConfig;
